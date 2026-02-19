2286af35079184ed339de08c43e30558fa0997ce
Tamilarasi <mtamilarasi589@gmail.com>
2026-02-19 12:38:40 +0530

    updation

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,7 @@ import os
 
 # Add app directory to sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../app')))
-from app import app
+from app.app import app
 
 @pytest.fixture
 def client():
